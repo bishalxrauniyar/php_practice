8a89506fab271191e0aa1fb3f4943316218a7ea9
bishalxrauniyar <Bishal.rauniyar@gmail.com>
2024-12-17 13:58:30 +0545

    Php basic get post variable ,basic methods etc

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+echo "Hello World! Bishal Rauniyar <br>";
+$name = "Bishal Rauniyar";
+$age = 21;
+$gpa = 3.5;
+$isMale = true;
+$food = "pizza";
+$quantity = 2;
+$price = 30.5;
+$total = $quantity * $price;
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,9 +21,51 @@
 </head>
 
 <body>
-    <?php
-    echo "Hello World!";
-    ?>
+
+    <!-- <h2>hello <?php echo $name ?></h2>
+    <p><?php echo "$name has ordered $quantity $food online <br>"; ?></p>
+    <p><?php echo "your total is RS {$quantity}*{$price}={$total}"; ?></p> -->
+    <!-- sign up form -->
+
+    <!-- POST METHOD -->
+    <!-- <form action="index.php" method="post">
+
+        <input type="email" name="email" placeholder="Enter your email"><br>
+        <input type="password" name="password" placeholder="Enter your password"><br>
+        <button type="submit">Sign In</button>
+
+    </form> -->
+
+    <!-- GET METHOD -->
+    <!-- <form action="index.php" method="get">
+
+        <input type="email" name="email" placeholder="Enter your email"><br>
+        <input type="password" name="password" placeholder="Enter your password"><br>
+        <button type="submit">Sign Up</button>
+
+    </form> -->
+
+    <!-- <form action="index.php" method="post">
+        <label for="Pizza">Pizza Quantity</label>
+        <input type="number" name="pizza">
+        <input type="submit" value="Submit">
+    </form> -->
+
 </body>
 
 </html>
+
+<?php
+// post method
+// echo "{$_POST["email"]} <br>";
+// echo "{$_POST["password"]} <br>";
+
+// get method
+// echo "{$_GET["email"]} <br>";
+// echo "{$_GET["password"]} <br>";
+// $pizzaQantity = $_POST["pizza"];
+// $total = $pizzaQantity * $price;
+// echo "You have ordered $pizzaQantity pizza";
+// echo "<br>";
+// echo "Your total is RS $pizzaQantity * $price = {$total}";
+?>
